9057d0935ecbefca51b94715f5ccea82ce03cbbf
Alex Thorne <alexthorne90@gmail.com>
2017-07-28 13:00:04 +0100

    Test code in main to print sensor values

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <Arduino.h>
+#include "metal_detector_lib.h"
+
+static MetalDetector metal_detector;
 
 void setup()
 {
-	Serial.begin(115200);
-	delay(1000);
+    Serial.begin(115200);
+    metal_detector.ConnectToSensor();
+    delay(500);
+    metal_detector.ConfigureSensor();
+    delay(500);
 }
 
 void loop()
 {
-	Serial.println("Hello world");
-	delay(1000);
+    metal_detector.Update();
+    Serial.print("Frequency value ");
+    Serial.println(metal_detector.GetCh0FreqHz());
+    Serial.print("Inductance value ");
+    Serial.println(metal_detector.GetCh0InductanceuH(), 6);
+    delay(500);
 }
