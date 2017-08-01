d46aca1aa1ab8724644fbb4ea10153b36b3ba4ea
Alex Thorne <alexthorne90@gmail.com>
2017-08-01 09:57:34 +0100

    Sets up timing test in main loop
    Runs a function for a desired number of iterations, and keep track of
    the worst case and average runtimes.  Results printed out at the end.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <Arduino.h>
 
+static const long NUM_TESTS = 1000;
+static const long TIME_BETWEEN_TESTS_MS = 10;
+
+static long start_time = 0;
+static long end_time = 0;
+static long this_run_time = 0;
+static long worst_time = 0;
+static long total_time = 0;
+
 void setup()
 {
 	Serial.begin(115200);
@@ -9,5 +18,32 @@ void setup()
 void loop()
 {
 	Serial.println("Hello world");
-	delay(1000);
+    for (int i = 0; i < NUM_TESTS; i ++)
+    {
+        start_time = millis();
+
+        //function_to_test();
+
+        end_time = millis();
+        this_run_time = end_time - start_time;
+        if (this_run_time > worst_time)
+        {
+            worst_time = this_run_time;
+            Serial.print("New worst time ");
+            Serial.println(worst_time);
+        }
+        total_time += this_run_time;
+        delay(TIME_BETWEEN_TESTS_MS);
+    }
+
+    Serial.println();
+    Serial.println("--- Test results ---");
+    Serial.print("Number of iterations: ");
+    Serial.println(NUM_TESTS);
+    Serial.print("Worst case runtime (ms): ");
+    Serial.println(worst_time);
+    Serial.print("Average runtime (ms): ");
+    Serial.println(total_time / NUM_TESTS);
+
+    while (1);
 }
