4af04590c0cf30730e59f1ffddf83719a667c442
Alex Thorne <alexthorne90@gmail.com>
2017-08-11 11:22:33 +0100

    MetalScanner class created for defined area scanning
    The MetalScanner object executes a state machine via its Update function
    designed to scan an area defined by the given origin and desired
    horizontal and depth travel bounds.  It first moves to a distance above
    the origin, and desends straight downward until the desired inducance
    delta is reached.  It then scans along the x and y axes by the defined
    amounts, correcting the height based on the desired inductance along the
    entire route.
    
    Iniitialization of the object is done through a HardwareSetup function
    to connect all hardware, and a SetReferenceInductance function to cache
    the reference inducatnce value.
    
    Setters are included for all parameters, as well as an isScanComplete
    function and a ResetScan function so that it can be automatically
    repeated.
    
    Link to lucidchart state machine.
    https://www.lucidchart.com/documents/edit/8c9fd1a9-f56d-4d4b-9284-004321b2eb55#

---
+++ src/metal_scanner.cpp
@@ -0,0 +1,337 @@
+/**
+ * @file metal_scanner_lib.cpp
+ * @author Alex Thorne
+ * @version 1.0
+ */
+
+#include "metal_scanner.h"
+
+//#define METAL_SCANNER_DEBUG
+
+MetalScanner::MetalScanner(void) : ArmController()
+{
+	//initialize
+    reference_inductance = 0;
+    desired_delta_uH = DEFAULT_DELTA_uH;
+    uH_tolerance = DEFAULT_uH_TOLERANCE;
+    horizontal_desired_travel_mm = DEFAULT_HORIZONTAL_DESIRED_TRAVEL_MM;
+    depth_desired_travel_mm = DEFAULT_DEPTH_DESIRED_TRAVEL_MM;
+    depth_scan_increment_mm = DEFAULT_DEPTH_SCAN_INCREMENT_MM;
+    travel_increment_mm = DEFAULT_TRAVEL_INCREMENT_MM;
+    height_correction_mm = DEFAULT_HEIGHT_CORRECTION_MM;
+    current_state = S0_IDLE;
+    next_state = S0_IDLE;
+    transitioned_state = false;
+}
+
+void MetalScanner::HardwareSetup(void)
+{
+    metal_detector.ConnectToSensor();
+    metal_detector.ConfigureSensor();
+    AttachMotors();
+}
+
+void MetalScanner::SetReferenceInductance(void)
+{
+    metal_detector.Update();
+    reference_inductance = metal_detector.GetCh0InductanceuH();
+}
+
+uint8_t MetalScanner::SetScanOrigin(float x, float y, float z)
+{
+    origin_x = x;
+    origin_y = y;
+    origin_z = z;
+    return 0;
+}
+
+uint8_t MetalScanner::SetDesiredInductanceDelta(float inductance)
+{
+    desired_delta_uH = inductance;
+    return 0;
+}
+
+uint8_t MetalScanner::SetInductanceDeltaTolerance(float tolerance)
+{
+    uH_tolerance = tolerance;
+    return 0;
+}
+
+uint8_t MetalScanner::SetDesiredHorizontalTravelMM(float horizontal_travel)
+{
+    horizontal_desired_travel_mm = horizontal_travel;
+    return 0;
+}
+
+uint8_t MetalScanner::SetDesiredDepthTravelMM(float depth_travel)
+{
+    depth_desired_travel_mm = depth_travel;
+    return 0;
+}
+
+uint8_t MetalScanner::SetDepthScanIncrementMM(float depth_increment)
+{
+    depth_scan_increment_mm = depth_increment;
+    return 0;
+}
+
+uint8_t MetalScanner::SetTravelIncrementMM(float travel_increment)
+{
+    travel_increment_mm = travel_increment;
+    return 0;
+}
+
+uint8_t MetalScanner::SetHeightCorrectionMM(float height_correction)
+{
+    height_correction_mm = height_correction;
+    return 0;
+}
+
+void MetalScanner::Update(uint16_t time_since_last_update_ms)
+{
+    metal_detector.Update();
+    current_inductance = metal_detector.GetCh0InductanceuH();
+    inductance_delta = reference_inductance - current_inductance;
+    current_x = GetCurrentX();
+    current_y = GetCurrentY();
+    current_z = GetCurrentZ();
+
+    switch (current_state)
+    {
+        case S0_IDLE:
+            next_state = S0_Run();
+            break;
+        case S1_MOVE_ABOVE_ORIGIN:
+            next_state = S1_Run();
+            break;
+        case S2_FIND_HEIGHT:
+            next_state = S2_Run();
+            break;
+        case S3_HORIZONTAL_MOVE_FWD:
+            next_state = S3_Run();
+            break;
+        case S4_DEPTH_MOVE:
+            next_state = S4_Run();
+            break;
+        case S5_HORIZONTAL_MOVE_BACK:
+            next_state = S5_Run();
+            break;
+        case S6_CYCLE_END:
+            next_state = S6_Run();
+            break;
+        case S7_SCAN_FINISHED:
+            next_state = S7_Run();
+            break;
+        default:
+            break;
+    }
+    if (current_state != next_state)
+    {
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Transitioning to state ");
+        Serial.println(next_state);
+#endif
+        transitioned_state = true;
+    }
+    else
+    {
+        transitioned_state = false;
+    }
+    current_state = next_state;
+
+    ArmController::Update(time_since_last_update_ms);
+}
+
+bool MetalScanner::isScanComplete(void)
+{
+    return (current_state == S7_SCAN_FINISHED);
+}
+
+void MetalScanner::ResetScan(void)
+{
+    current_state = S0_IDLE;
+    next_state = S0_IDLE;
+}
+
+// Private functions ***********************************************************
+float MetalScanner::GetCorrectedHeight(float current_z, float inductance)
+{
+    float corrected_z = current_z;
+    if (inductance >= (desired_delta_uH + uH_tolerance))
+    {
+        corrected_z += height_correction_mm;
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Inductance corrected height up to ");
+        Serial.println(corrected_z);
+#endif
+    }
+    if (inductance <= (desired_delta_uH - uH_tolerance))
+    {
+        corrected_z -= height_correction_mm;
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Inductance corrected height down to ");
+        Serial.println(corrected_z);
+#endif
+    }
+    return corrected_z;
+}
+
+MetalScanner::scan_state MetalScanner::S0_Run(void)
+{
+    current_cycle_count = 1;
+    return S1_MOVE_ABOVE_ORIGIN;
+}
+
+MetalScanner::scan_state MetalScanner::S1_Run(void)
+{
+    if (transitioned_state)
+    {
+        SetArm(origin_x, origin_y, origin_z + DISTANCE_ABOVE_ORIGIN_START_MM,
+                -90);
+    }
+
+    if (hasReachedDesiredPosition())
+    {
+        return S2_FIND_HEIGHT;
+    }
+    else
+    {
+        return S1_MOVE_ABOVE_ORIGIN;
+    }
+}
+
+MetalScanner::scan_state MetalScanner::S2_Run(void)
+{
+    if (hasReachedDesiredPosition())
+    {
+        SetArm(origin_x, origin_y, current_z - travel_increment_mm, -90);
+    }
+
+    if (inductance_delta > desired_delta_uH)
+    {
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Height found at z = ");
+        Serial.print(current_z - travel_increment_mm);
+        Serial.print(" current inductance = ");
+        Serial.print(current_inductance);
+        Serial.print(" reference = ");
+        Serial.println(reference_inductance);
+#endif
+        return S3_HORIZONTAL_MOVE_FWD;
+    }
+    else
+    {
+        return S2_FIND_HEIGHT;
+    }
+}
+
+MetalScanner::scan_state MetalScanner::S3_Run(void)
+{
+    if (current_x >= (horizontal_desired_travel_mm + origin_x))
+    {
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Horizontal fwd move completed at x = ");
+        Serial.print(current_x);
+        Serial.print(" y = ");
+        Serial.print(current_y);
+        Serial.print(" z = ");
+        Serial.println(current_z);
+#endif
+        return S4_DEPTH_MOVE;
+    }
+
+    if (hasReachedDesiredPosition())
+    {
+        SetArm(current_x + travel_increment_mm, current_y, GetCorrectedHeight(
+                    current_z, inductance_delta), -90);
+    }
+
+    return S3_HORIZONTAL_MOVE_FWD;
+}
+
+MetalScanner::scan_state MetalScanner::S4_Run(void)
+{
+    if (current_y >= ((depth_scan_increment_mm * current_cycle_count) +
+                origin_y))
+    {
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Depth move completed at x = ");
+        Serial.print(current_x);
+        Serial.print(" y = ");
+        Serial.print(current_y);
+        Serial.print(" z = ");
+        Serial.println(current_z);
+#endif
+        return S6_CYCLE_END;
+    }
+
+    if (hasReachedDesiredPosition())
+    {
+        SetArm(current_x, current_y + travel_increment_mm, GetCorrectedHeight(
+                    current_z, inductance_delta), -90);
+    }
+
+    return S4_DEPTH_MOVE;
+}
+
+MetalScanner::scan_state MetalScanner::S5_Run(void)
+{
+    if (current_x <= origin_x)
+    {
+#ifdef METAL_SCANNER_DEBUG
+        Serial.print("Horizontal back move completed at x = ");
+        Serial.print(current_x);
+        Serial.print(" y = ");
+        Serial.print(current_y);
+        Serial.print(" z = ");
+        Serial.println(current_z);
+#endif
+        return S4_DEPTH_MOVE;
+    }
+
+    if (hasReachedDesiredPosition())
+    {
+        SetArm(current_x - travel_increment_mm, current_y, GetCorrectedHeight(
+                    current_z, inductance_delta), -90);
+    }
+
+    return S5_HORIZONTAL_MOVE_BACK;
+}
+
+MetalScanner::scan_state MetalScanner::S6_Run(void)
+{
+    if (transitioned_state)
+    {
+        current_cycle_count ++;
+    }
+
+    if ((current_y < (depth_desired_travel_mm + origin_y)) &&
+            (current_x >= (horizontal_desired_travel_mm + origin_x)))
+    {
+        return S5_HORIZONTAL_MOVE_BACK;
+    }
+    else if ((current_y < (depth_desired_travel_mm + origin_y)) &&
+            (current_x <= origin_x))
+    {
+        return S3_HORIZONTAL_MOVE_FWD;
+    }
+    else if (current_y >= (depth_desired_travel_mm + origin_y))
+    {
+        return S7_SCAN_FINISHED;
+    }
+    else
+    {
+        return S6_CYCLE_END;
+    }
+}
+
+MetalScanner::scan_state MetalScanner::S7_Run(void)
+{
+#ifdef METAL_SCANNER_DEBUG
+    if (transitioned_state)
+    {
+        Serial.println("Scan complete!");
+    }
+#endif
+    return S7_SCAN_FINISHED;
+}
---
+++ src/metal_scanner.h
@@ -0,0 +1,96 @@
+/**
+ * @file metal_scanner_lib.h
+ * @author Alex Thorne
+ * @version 1.0
+ */
+
+#ifndef METAL_SCANNER_LIB_H
+#define METAL_SCANNER_LIB_H
+
+#include <Arduino.h>
+#include "arm_controller.h"
+#include "metal_detector_lib.h"
+
+class MetalScanner : public ArmController {
+
+    public:
+
+        //Constructor
+        MetalScanner(void);
+
+        //Public interface
+        void HardwareSetup(void);
+        void SetReferenceInductance(void);
+        uint8_t SetScanOrigin(float x, float y, float z);
+        uint8_t SetDesiredInductanceDelta(float inductance);
+        uint8_t SetInductanceDeltaTolerance(float tolerance);
+        uint8_t SetDesiredHorizontalTravelMM(float horizontal_travel);
+        uint8_t SetDesiredDepthTravelMM(float depth_travel);
+        uint8_t SetDepthScanIncrementMM(float depth_increment);
+        uint8_t SetTravelIncrementMM(float travel_increment);
+        uint8_t SetHeightCorrectionMM(float height_correction);
+        void Update(uint16_t time_since_last_update_ms);
+        bool isScanComplete(void);
+        void ResetScan(void);
+
+    private:
+
+        typedef enum scan_state {
+            S0_IDLE,
+            S1_MOVE_ABOVE_ORIGIN,
+            S2_FIND_HEIGHT,
+            S3_HORIZONTAL_MOVE_FWD,
+            S4_DEPTH_MOVE,
+            S5_HORIZONTAL_MOVE_BACK,
+            S6_CYCLE_END,
+            S7_SCAN_FINISHED
+        } scan_state;
+
+        //Private variables
+        MetalDetector metal_detector;
+        float reference_inductance;
+        float origin_x;
+        float origin_y;
+        float origin_z;
+        float desired_delta_uH;
+        float uH_tolerance;
+        float horizontal_desired_travel_mm;
+        float depth_desired_travel_mm;
+        float depth_scan_increment_mm;
+        float travel_increment_mm;
+        float height_correction_mm;
+        scan_state current_state;
+        scan_state next_state;
+        bool transitioned_state;
+        uint16_t current_cycle_count;
+        float current_inductance;
+        float inductance_delta;
+        float current_x;
+        float current_y;
+        float current_z;
+
+        //Default settings
+        const float DEFAULT_DELTA_uH = 0.25;
+        const float DEFAULT_uH_TOLERANCE = 0.1;
+        const float DEFAULT_HORIZONTAL_DESIRED_TRAVEL_MM = 50;
+        const float DEFAULT_DEPTH_DESIRED_TRAVEL_MM = 25;
+        const float DEFAULT_DEPTH_SCAN_INCREMENT_MM = 5;
+        const float DEFAULT_TRAVEL_INCREMENT_MM = 1.0;
+        const float DEFAULT_HEIGHT_CORRECTION_MM = 1.0;
+        const float DISTANCE_ABOVE_ORIGIN_START_MM = 60.0;
+
+        //Private helper functions
+        float GetCorrectedHeight(float current_z, float inductance);
+        scan_state S0_Run(void);
+        scan_state S1_Run(void);
+        scan_state S2_Run(void);
+        scan_state S3_Run(void);
+        scan_state S4_Run(void);
+        scan_state S5_Run(void);
+        scan_state S6_Run(void);
+        scan_state S7_Run(void);
+};
+
+#endif
+
+
