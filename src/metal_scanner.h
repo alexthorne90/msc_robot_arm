4a3632c8a9d945c49590d109d9349117a6ce0560
Alex Thorne <alexthorne90@gmail.com>
2017-08-25 16:27:01 +0100

    Makes MetalScanner an abstract class
    This will now be the base ABC for all future scanners.  The Update
    function is now virtual, forcing all derived scanners to implement their
    own.  isScanComplete(), resetScan(), and GetOutOfBoundsPercentage() also
    virtual.
    
    Removes much of the defined-area specific functionality, leaving only
    base necessities for any generic scanner.  All variables in the scanner
    are protected rather than private.

--- src/metal_scanner.h
+++ src/metal_scanner.h
@@ -24,28 +24,12 @@ class MetalScanner : public ArmController {
         uint8_t SetScanOrigin(float x, float y, float z);
         uint8_t SetDesiredInductanceDelta(float inductance);
         uint8_t SetInductanceDeltaTolerance(float tolerance);
-        uint8_t SetDesiredHorizontalTravelMM(float horizontal_travel);
-        uint8_t SetDesiredDepthTravelMM(float depth_travel);
-        uint8_t SetDepthScanIncrementMM(float depth_increment);
-        uint8_t SetTravelIncrementMM(float travel_increment);
-        uint8_t SetHeightCorrectionMM(float height_correction);
-        void Update(uint16_t time_since_last_update_ms);
-        bool isScanComplete(void);
-        void ResetScan(void);
-        float GetOutOfBoundsPercentage(void);
+        virtual void Update(uint16_t time_since_last_update_ms) = 0;
+        virtual bool isScanComplete(void) = 0;
+        virtual void ResetScan(void) = 0;
+        virtual float GetOutOfBoundsPercentage(void) = 0;
 
-    private:
-
-        typedef enum scan_state {
-            S0_IDLE,
-            S1_MOVE_ABOVE_ORIGIN,
-            S2_FIND_HEIGHT,
-            S3_HORIZONTAL_MOVE_FWD,
-            S4_DEPTH_MOVE,
-            S5_HORIZONTAL_MOVE_BACK,
-            S6_CYCLE_END,
-            S7_SCAN_FINISHED
-        } scan_state;
+    protected:
 
         //Private variables
         MetalDetector metal_detector;
@@ -55,31 +39,10 @@ class MetalScanner : public ArmController {
         float origin_z;
         float desired_delta_uH;
         float uH_tolerance;
-        float horizontal_desired_travel_mm;
-        float depth_desired_travel_mm;
-        float depth_scan_increment_mm;
-        float travel_increment_mm;
-        float height_correction_mm;
-        scan_state current_state;
-        scan_state next_state;
-        bool transitioned_state;
-        uint16_t current_cycle_count;
-        float current_inductance;
-        float inductance_delta;
-        float current_x;
-        float current_y;
-        float current_z;
-        uint32_t move_counter;
-        uint32_t out_of_bounds_counter;
 
         //Default settings
         const float DEFAULT_DELTA_uH = 1.0;
         const float DEFAULT_uH_TOLERANCE = 0.4;
-        const float DEFAULT_HORIZONTAL_DESIRED_TRAVEL_MM = 0;
-        const float DEFAULT_DEPTH_DESIRED_TRAVEL_MM = 0;
-        const float DEFAULT_DEPTH_SCAN_INCREMENT_MM = 0;
-        const float DEFAULT_TRAVEL_INCREMENT_MM = 0.5;
-        const float DEFAULT_HEIGHT_CORRECTION_MM = 1.0;
         const float DISTANCE_ABOVE_ORIGIN_START_MM = 60.0;
 
         //Boundaries
@@ -89,20 +52,6 @@ class MetalScanner : public ArmController {
         const float Y_MAX_BOUNDARY = 230.0;
         const float Z_MIN_BOUNDARY = 5.0;
         const float Z_MAX_BOUNDARY = 100.0;
-        const float MAX_TRAVEL_INCREMENT = 14.0;
-        const float MAX_HEIGHT_ADJUSTMENT = 5.0;
-
-        //Private helper functions
-        float GetCorrectedHeight(float current_z, float inductance);
-        scan_state S0_Run(void);
-        scan_state S1_Run(void);
-        scan_state S2_Run(void);
-        scan_state S3_Run(void);
-        scan_state S4_Run(void);
-        scan_state S5_Run(void);
-        scan_state S6_Run(void);
-        scan_state S7_Run(void);
-        bool isScanAreaValid(void);
 };
 
 #endif
