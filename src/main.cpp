7e8b903ae77834d1a3acbdd6ad8d1bc7444f1801
Alex Thorne <alexthorne90@gmail.com>
2017-10-19 15:23:12 +0100

    Main prepared for use of scanners
    Set up for the ShapeMappingScanner, with commented out portions for easy
    switch over to the DefinedAreaScanner.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
 #include "scheduler.h"
+#include "shape_mapping_scanner.h"
+#include "defined_area_scanner.h"
 
 static Scheduler scheduler;
 static const int UPDATE_PERIOD_MS = 50;
+static void AppUpdate(void);
+
+// Scanner objects
+// Switch the commented out portions for other type of scanning
+static ShapeMappingScanner scanner;
+//static DefinedAreaScanner scanner;
 
 void setup()
 {
 	Serial.begin(115200);
-	delay(1000);
-	Serial.println("Hello world");
-    //scheduler.ScheduleFunction(TASK, UPDATE_PERIOD_MS);
+    scheduler.ScheduleFunction(AppUpdate, UPDATE_PERIOD_MS);
+    scanner.HardwareSetup();
+    delay(1000);
+    scanner.SetReferenceInductance();
+    scanner.SetHomePosition();
+    scanner.SetScanOrigin(0, 150, 40);
+
+    // Uncomment these for use of the DefinedAreaScanner
+    // and update parameters if necessary
+    //scanner.SetDesiredHorizontalTravelMM(100);
+    //scanner.SetDesiredDepthTravelMM(40);
+    //scanner.SetDepthScanIncrementMM(5);
+
 }
 
 void loop()
 {
     scheduler.Update();
+}
+
+static void AppUpdate(void)
+{
+    scanner.Update(UPDATE_PERIOD_MS);
+    if (scanner.isScanComplete())
+    {
+        Serial.println("Scan complete!");
+        while(1);
+    }
 }
